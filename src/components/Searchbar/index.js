c48768be33f008606556652ef92cc54cf4c1585a
bacarybruno <bacarybruno@gmail.com>
2019-01-03 09:33:08 +0400

    changed searchbar container background

--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
 import {
-    Paper, InputBase, Divider, IconButton
+    Paper, InputBase, Divider, IconButton, Grid
 } from '@material-ui/core';
 import {
     Search as SearchIcon, KeyboardArrowDown as FilterIcon
@@ -11,16 +11,24 @@ import {
 const Searchbar = (props) => {
     const classes = props.classes;
     return (
-        <Paper className={classes.root} elevation={1}>
-            <InputBase onChange={(event) => console.log(event.target.value)} className={classes.input} placeholder="Search News" />
-            <IconButton onClick={() => console.log('yes') } className={classes.iconButton} aria-label="Search">
-                <SearchIcon />
-            </IconButton>
-            <Divider className={classes.divider} />
-            <IconButton color="primary" className={classes.iconButton} aria-label="Directions">
-                <FilterIcon />
-            </IconButton>
-        </Paper>
+        <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            className={classes.container}
+        >
+            <Paper className={classes.root} elevation={1}>
+                <InputBase onChange={(event) => console.log(event.target.value)} className={classes.input} placeholder="Search News" />
+                <IconButton onClick={() => console.log('yes')} className={classes.iconButton} aria-label="Search">
+                    <SearchIcon />
+                </IconButton>
+                <Divider className={classes.divider} />
+                <IconButton color="primary" className={classes.iconButton} aria-label="Directions">
+                    <FilterIcon />
+                </IconButton>
+            </Paper>
+        </Grid>
     );
 }
 
--- src/components/Searchbar/styles.js
+++ src/components/Searchbar/styles.js
@@ -1,8 +1,12 @@
 const styles = {
-    root: {
-        marginLeft: 'auto',
-        marginRight: 'auto',
+    container: {
+        height: 300,
+        backgroundImage: 'url(https://ssl.gstatic.com/support/content/images/static/homepage_header_background.svg)',
+        backgroundPosition: '50%',
+        backgroundRepeat: 'no-repeat',
         marginTop: 30,
+    },
+    root: {
         padding: '2px 4px',
         display: 'flex',
         alignItems: 'center',
@@ -11,6 +15,7 @@ const styles = {
     input: {
         marginLeft: 8,
         flex: 1,
+        paddingLeft: 15,
     },
     iconButton: {
         padding: 10,
